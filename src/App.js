a3b5f6361f6d4310d02169678ec5ed7902945a78
Ross <rosshamey@gmail.com>
2022-12-15 12:00:50 -0500

    started work on cv

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { useState } from 'react';
 import './App.css';
-import General from './components/general'
-import Experience from './components/experience'
 import Education from './components/education'
-import Form from './components/form'
 
 
 function App() {
+
   const [message, setMessage] = useState('');
   const handleChange = event => {
     setMessage(event.target.value);
@@ -18,10 +16,7 @@ function App() {
 
   return (
     <div className="App">
-        <General></General>
-        <Experience></Experience>
         <Education></Education>
-        <Form></Form>
     </div>
   );
 }
--- src/components/education.jsx
+++ src/components/education.jsx
@@ -27,13 +27,50 @@ export default class About extends Component {
    render() {
       return (
             <div>
-               <div>EDUCATION</div>
-               <input name="name" type="text" placeholder="Input name..." id="name" onChange={this.handleInputChange} value={this.state.name}></input>
-               <input name="email" placeholder="Input email..." id="email" onChange={this.handleInputChange} value={this.state.email}></input>
-               <input name="mobile" placeholder="Input mobile..." id="mobile" onChange={this.handleInputChange} value={this.state.mobile}></input>
-               <p>{this.state.name}</p>
-               <p>{this.state.email}</p>
-               <p>{this.state.mobile}</p>
+               <div class="generalInfo">GENERAL INFO
+                  <div class="infoCont">
+                     <input name="name" type="text" placeholder="Input name..." id="name" onChange={this.handleInputChange} value={this.state.name}></input>
+                     <input name="email" placeholder="Input email..." id="email" onChange={this.handleInputChange} value={this.state.email}></input>
+                     <input name="mobile" placeholder="Input mobile..." id="mobile" onChange={this.handleInputChange} value={this.state.mobile}></input>
+                     <input name="date" placeholder="Input date..." id="mobile" onChange={this.handleInputChange} value={this.state.date}></input>
+                  </div>
+               </div>
+
+               <div class="education">EDUCATION
+                  <div class="infoCont">
+                     <input name="school" type="text" placeholder="Input School / Uni..." id="name" onChange={this.handleInputChange} value={this.state.school}></input>
+                     <input name="study" placeholder="Input Major / Study..." id="study" onChange={this.handleInputChange} value={this.state.study}></input>
+                     <input name="startDateS" placeholder="Input start date..." id="startDateS" onChange={this.handleInputChange} value={this.state.startDateS}></input>
+                     <input name="endDateS" placeholder="Input end date..." id="endDateS" onChange={this.handleInputChange} value={this.state.endDateS}></input>
+                  </div>
+               </div>
+
+               <div class="experience">EXPERIENCE
+                  <div class="infoCont">
+                     <input name="position" type="text" placeholder="Input position..." id="name" onChange={this.handleInputChange} value={this.state.position}></input>
+                     <input name="company" placeholder="Input company..." id="study" onChange={this.handleInputChange} value={this.state.company}></input>
+                     <input name="startDateW" placeholder="Input start date..." id="startDateS" onChange={this.handleInputChange} value={this.state.startDateW}></input>
+                     <input name="endDateW" placeholder="Input end date..." id="endDateS" onChange={this.handleInputChange} value={this.state.endDateW}></input>
+                  </div>
+               </div>
+
+               <div class="cvBuilder">
+                  <div class="headerCont">
+                     <div class="header">
+                        <div class="leftHead">
+                           <div class="name">
+                           {this.state.name}
+                           </div>
+                           <div class="date">
+                           {this.state.date}
+                           </div>
+                        </div>
+                        <div class="rightHead">
+
+                        </div>
+                     </div>
+                  </div>
+               </div>
             </div>
       );
    }
